35216d11775fb6ca8d68004c14f656a39ed257a3
Prajiet103 <u3334297@uni.canberra.edu.au>
2026-03-26 17:01:46 +1100

    Add files via upload

---
+++ tutorial week 6.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+calc = tk.Tk()           # create the main window
+calc.title("calculator")
+calc.geometry("320x320") # width x height (optional)
+
+def calculate(a,b,op):
+    return (a
+
+calc.mainloop()          # start the event loop
